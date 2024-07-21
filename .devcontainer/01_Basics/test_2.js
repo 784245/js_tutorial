6fce268ad0b57da1b7c7af9cae73cbab231b3d08
Suhail Bashir <98446219+784245@users.noreply.github.com>
2024-07-21 02:03:48 +0000

    datatypes

---
+++ .devcontainer/01_Basics/test_2.js
@@ -0,0 +1,7 @@
+let a=33
+let stringNumber=String(a)
+let booleanNumber=Boolean(stringNumber)
+console.log(typeof(booleanNumber));
+console.log(booleanNumber)
+console.log(typeof a);
+console.log(typeof stringNumber);
